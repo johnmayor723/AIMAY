1b38367613eda437dbe98209868156666c0b8ac3
may <may@xymail.com>
2025-12-11 17:55:45 +0100

    nbb

--- routes/attendeeRoutes.js
+++ routes/attendeeRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { homePage, register, verifyPage, verifyCode } from "../controllers/attendeeController.js";
+import { homePage, register, verifyPage, verifyCode, getAllAttendees } from "../controllers/attendeeController.js";
 
 const router = express.Router();
 
@@ -7,5 +7,6 @@ router.get("/", homePage);
 router.post("/register", register);
 router.get("/verify", verifyPage);
 router.post("/verify", verifyCode);
+router.get("/all", getAllAttendees )
 
 export default router;
